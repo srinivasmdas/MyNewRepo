26b656c1ffc049bf272b66fe7a4f5cdc9ea1c2ca
Srinivas M <srinivas.m.das@gmail.com>
2017-06-04 12:15:36 +0000

    Adding Region to code

--- ec2listinstance.py
+++ ec2listinstance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import boto3
 
-ec2 = boto3.resource('ec2')
+ec2 = boto3.resource('ec2', region_name='us-east-1')
 for myinstancelist in ec2.instances.all():
     print myinstancelist.id, myinstancelist.state
 
