35dbe465e34648fda39ff2e22c93176853d1b304
Srinivas M <srinivas.m.das@gmail.com>
2017-06-04 18:50:14 +0000

    adding region configuration

--- createsnstopic.py
+++ createsnstopic.py
@@ -3,7 +3,7 @@
 import boto3
 import sys
 
-SNS = boto3.client('sns')
+SNS = boto3.client('sns', region_name='us-east-1')
 for topicname in sys.argv[1:]:
     response = SNS.create_topic(Name=topicname)
     print response
