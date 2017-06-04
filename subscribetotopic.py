03d978a65f8dbf5a5bbecad7d7dc49d1ff47eaac
Srinivas M <srinivas.m.das@gmail.com>
2017-06-04 18:09:35 +0000

    Adding more features

---
+++ subscribetotopic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import boto3
+import sys
+
+SNS = boto3.client('sns')
+for topicarnname in sys.argv[1:]:
+    response = SNS.subscribe(
+    TopicArn=topicarnname,
+    Protocol='email',
+    Endpoint='srinivas.m.das@gmail.com')
+    print response
