706c23aee6b6fbd072c92dead39ab3f3d2d344c4
Srinivas M <srinivas.m.das@gmail.com>
2017-06-04 18:54:53 +0000

    fixing syntax error

--- deletesnstopic.py
+++ deletesnstopic.py
@@ -3,7 +3,7 @@
 import boto3
 import sys
 
-SNS = boto3.client('sns'i, region_name='us-east-1')
+SNS = boto3.client('sns', region_name='us-east-1')
 
 for snstopicarn in sys.argv[1:]:
     response = SNS.delete_topic(
