b7bda8c3e0b85e5c1bbed12536b62e84e35191c2
Srinivas M <srinivas.m.das@gmail.com>
2017-06-05 09:22:53 +0000

    adding new featuer

---
+++ enabletermination.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import boto3
+import sys
+
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+for myinstance in sys.argv[1:]:
+    instance = ec2.Instance(myinstance)
+    response = instance.modify_attribute(
+        Attribute='disableApiTermination',
+        Value='False')
+print response
